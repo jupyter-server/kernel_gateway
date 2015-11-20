8daad4f0968e4bd4062f24e3b5602e9d9745b492
Corey A. Stubbs <cstubbs@us.ibm.com>
2015-11-20 15:18:07 -0600

    [Issue 29]: Initial http api support for notebooks Can add GET, POST, PUT, and DELETE method from notebok. Can access request body in cells (c) Copyright IBM Corp. 2015

---
+++ kernel_gateway/services/notebooks/handlers.py
@@ -0,0 +1,83 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import tornado.web
+import json
+from queue import Empty
+class NotebookAPIHandler(tornado.web.RequestHandler):
+    get_source = None
+    put_source = None
+    post_source = None
+    delete_source = None
+    kernel_client = None
+    execution_timeout = 5
+    def initialize(self, sources, kernel_client):
+        self.kernel_client = kernel_client
+        if 'GET' in sources:
+            self.get_source = sources['GET']
+        if 'POST' in sources:
+            self.post_source = sources['POST']
+        if 'PUT' in sources:
+            self.put_source = sources['PUT']
+        if 'DELETE' in sources:
+            self.delete_source = sources['DELETE']
+
+    def _send_code(self, code, block=True):
+        # execute the get_source
+        msg_id = self.kernel_client.execute(code)
+        # This message will contain the status of code execution (failure or success)
+        result_found = False
+        iopub_message = None
+        result = None
+        try:
+            while(result is None):
+                iopub_message = self.kernel_client.get_iopub_msg(block=block, timeout=self.execution_timeout)
+                # print(iopub_message)
+                if iopub_message['header']['msg_type'] == 'execute_result':
+                    result = iopub_message['content']['data']
+        except Empty:
+            print('Never found execute result')
+            pass
+
+        return result
+
+    def _handle_request(self, source_code):
+        if source_code is None:
+            self.set_status(405)
+            self.finish()
+
+            return
+
+        body = self.request.body.decode(encoding='UTF-8')
+        # TODO: Add query params to the request 
+        REQUEST = json.dumps({
+            'body' : json.loads(body) if body is not None and body is not '' else ''
+        })
+        request_code = "REQUEST = '"  + REQUEST + "'"
+
+        # TODO: Need to figure out multil-lang assignment
+        self._send_code(request_code, False)
+        result = self._send_code(source_code)
+
+        self.set_header('Content-Type', 'application/json')
+        if result is not None:
+            self.write(result)
+        self.set_status(200)
+        self.finish()
+
+    def get(self):
+        # execute the get_source
+        self._handle_request(self.get_source)
+
+    def post(self):
+        # execute the post_source
+        self._handle_request(self.post_source)
+
+    def put(self):
+        # execute the put_source
+        print('in put method')
+        self._handle_request(self.put_source)
+
+    def delete(self):
+        print('in delete method')
+        # execute the delete_source
+        self._handle_request(self.delete_source)
