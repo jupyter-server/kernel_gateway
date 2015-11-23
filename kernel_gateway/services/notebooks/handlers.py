c1875a761ef72cba28526d62725b6105034b8720
Corey A. Stubbs <cstubbs@us.ibm.com>
2015-11-23 16:58:23 -0600

    [Issue 29]: Refactor and clean up. Add logging to handlers and some error handling from kernel Add more noteboks with examples. Add Scotch demo API. (c) Copyright IBM Corp. 2015

--- kernel_gateway/services/notebooks/handlers.py
+++ kernel_gateway/services/notebooks/handlers.py
@@ -1,6 +1,7 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 import tornado.web
+from tornado.log import access_log
 import json
 from queue import Empty
 from .request_utils import *
@@ -11,7 +12,9 @@ class NotebookAPIHandler(tornado.web.RequestHandler):
     post_source = None
     delete_source = None
     kernel_client = None
+    # A timeout for executing code and getting a result back
     execution_timeout = 5
+
     def initialize(self, sources, kernel_client):
         self.kernel_client = kernel_client
         if 'GET' in sources:
@@ -24,26 +27,34 @@ class NotebookAPIHandler(tornado.web.RequestHandler):
             self.delete_source = sources['DELETE']
 
     def _send_code(self, code, block=True):
-        # execute the get_source
-        msg_id = self.kernel_client.execute(code)
-        # This message will contain the status of code execution (failure or success)
+        # execute the code
+        self.kernel_client.execute(code)
         result_found = False
         iopub_message = None
         result = None
+        status = 200
         try:
             while(result is None):
                 iopub_message = self.kernel_client.get_iopub_msg(block=block, timeout=self.execution_timeout)
-                # print(iopub_message)
                 if iopub_message['header']['msg_type'] == 'execute_result':
                     result = iopub_message['content']['data']
+                elif iopub_message['header']['msg_type'] == 'stream':
+                    result = iopub_message['content']['text']
+                elif iopub_message['header']['msg_type'] == 'error':
+                    result = 'Error {}: {} \n'.format(
+                        iopub_message['content']['ename'],
+                        iopub_message['content']['evalue']
+                    )
+                    status = 500
         except Empty:
-            print('Never found execute result')
+            access_log.debug('Never found execute result')
             pass
 
-        return result
+        return result, status
 
     def _handle_request(self, source_code):
         if source_code is None:
+            access_log.warn('No code found for method: {}'.format(self.request.method))
             self.set_status(405)
             self.finish()
             return
@@ -55,15 +66,16 @@ class NotebookAPIHandler(tornado.web.RequestHandler):
         })
         request_code = "REQUEST = '"  + REQUEST + "'"
 
-        print('REQUEST code is:\n ', request_code)
+        access_log.debug('Request code for notebook cell is: {}'.format(request_code))
         # TODO: Need to figure out multil-lang assignment
         self._send_code(request_code, False)
-        result = self._send_code(source_code)
+        result, status = self._send_code(source_code)
 
-        self.set_header('Content-Type', 'application/json')
+        # TODO: Will we need to add the ability to specify mime types?
+        self.set_header('Content-Type', 'text/plain')
         if result is not None:
             self.write(result)
-        self.set_status(200)
+        self.set_status(status)
         self.finish()
 
     def get(self, **kwargs):
@@ -76,10 +88,8 @@ class NotebookAPIHandler(tornado.web.RequestHandler):
 
     def put(self, **kwargs):
         # execute the put_source
-        print('in put method')
         self._handle_request(self.put_source)
 
     def delete(self, **kwargs):
-        print('in delete method')
         # execute the delete_source
         self._handle_request(self.delete_source)
