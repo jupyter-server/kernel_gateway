872042c84a4a2290ec1236b5c084a264838456d2
Carol Willing <carolcode@willingconsulting.com>
2016-03-05 02:19:24 -0800

    Fix indentation

--- kernel_gateway/tests/services/notebooks/test_request_utils.py
+++ kernel_gateway/tests/services/notebooks/test_request_utils.py
@@ -109,12 +109,12 @@ class TestRequestUtils(unittest.TestCase):
         self.assertEqual(result, {},'Empty headers handled incorrectly and did not make empty dict')
 
     def test_format_request_code_not_escaped(self):
-            '''Test formatting request code without escaped quotes'''
+        """Test formatting request code without escaped quotes"""
         test_request = ('{"body": "", "headers": {"Accept-Language": "en-US,en;q=0.8", '
                         '"If-None-Match": "9a28a9262f954494a8de7442c63d6d0715ce0998", '
                         '"Accept-Encoding": "gzip, deflate, sdch"}, "args": {}, "path": {}}')
         request_code = format_request(test_request)
-        #Get the value of REQUEST = "{ to test for equality
+
         test_request_js_value = request_code[request_code.index("\"{"):]
         self.assertEqual(test_request, json.loads(test_request_js_value), "Request code without escaped quotes was not formatted correctly")
 
@@ -124,6 +124,6 @@ class TestRequestUtils(unittest.TestCase):
                         '"If-None-Match": "\"\"9a28a9262f954494a8de7442c63d6d0715ce0998\"\"", '
                         '"Accept-Encoding": "gzip, deflate, sdch"}, "args": {}, "path": {}}')
         request_code = format_request(test_request)
-        #Get the value of REQUEST = "{ to test for equality
+
         test_request_js_value = request_code[request_code.index("\"{"):]
         self.assertEqual(test_request, json.loads(test_request_js_value), "Escaped Request code was not formatted correctly")
