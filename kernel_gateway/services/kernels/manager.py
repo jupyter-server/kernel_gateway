44a7a2079b0e5a480405f204c88271be39b72532
Michael Poplavski <mikepop77@gmail.com>
2016-03-24 09:26:27 -0500

    [Issue 131] - restart kernel on runtime error.
    (c) Copyright IBM Corp. 2016

--- kernel_gateway/services/kernels/manager.py
+++ kernel_gateway/services/kernels/manager.py
@@ -62,21 +62,30 @@ class SeedingMappingKernelManager(MappingKernelManager):
         """
         self.start_kernel(kernel_name=self.seed_kernelspec, *args, **kwargs)
 
-    def start_kernel(self, *args, **kwargs):
+    def start_kernel(self, restart_limit=3, *args, **kwargs):
         """Starts a kernel and then executes a list of code cells on it if a
         seed notebook exists.
         """
         kernel_id = super(MappingKernelManager, self).start_kernel(*args, **kwargs)
 
-        if kernel_id and self.seed_source is not None:
-            # Only run source if the kernel spec matches the notebook kernel spec
+        if kernel_id:
             kernel = self.get_kernel(kernel_id)
-            if kernel.kernel_name == self.seed_kernelspec:
-                # Create a client to talk to the kernel
-                client = kernel.client()
-                # Only start channels and wait for ready in HTTP mode
-                client.start_channels()
+            # Create a client to talk to the kernel
+            client = kernel.client()
+            # Only start channels and wait for ready in HTTP mode
+            client.start_channels()
+            try:
                 client.wait_for_ready()
+            except RuntimeError:
+                #shutdown kernel and clear connection file
+                self.shutdown_kernel(kernel_id)
+                #ensure that on recursive calls it will successfully start
+                print("restart limit is {}".format(restart_limit))
+                if restart_limit < 0:
+                    raise RuntimeError('Kernel restart limit reached')
+                return self.start_kernel(restart_limit-1, *args, **kwargs)
+            # Only run source if the kernel spec matches the notebook kernel spec
+            if kernel.kernel_name == self.seed_kernelspec and self.seed_source is not None:
                 for code in self.seed_source:
                     # Execute every non-API code cell and wait for each to
                     # succeed or fail
@@ -90,8 +99,8 @@ class SeedingMappingKernelManager(MappingKernelManager):
                             # Shutdown the kernel
                             self.shutdown_kernel(kernel_id)
                             raise RuntimeError('Error seeding kernel memory')
-                # Shutdown the channels to remove any lingering ZMQ messages
-                client.stop_channels()
+            # Shutdown the channels to remove any lingering ZMQ messages
+            client.stop_channels()
         return kernel_id
 
 class KernelGatewayIOLoopKernelManager(IOLoopKernelManager):
@@ -102,52 +111,3 @@ class KernelGatewayIOLoopKernelManager(IOLoopKernelManager):
     def _launch_kernel(self, kernel_cmd, **kw):
         kw['env']['KERNEL_GATEWAY'] = '1'
         return super(KernelGatewayIOLoopKernelManager, self)._launch_kernel(kernel_cmd, **kw)
-
-    def is_alive(self):
-        """Is the kernel process still running?"""
-        if self.has_kernel:
-            if self.kernel.poll() is None:
-                self.has_kernel_started = True
-                return True
-            else:
-                return False
-        else:
-            # we don't have a kernel
-            return False
-
-    def start_kernel(self, **kw):
-        self.has_kernel_started = False
-        super(KernelGatewayIOLoopKernelManager, self).start_kernel(**kw)
-
-    def restart_kernel(self, now=False, **kw):
-        """Restarts a kernel with the arguments that were used to launch it.
-        If the old kernel was launched and started with random ports,
-        the same ports will be used for the new kernel. The same connection
-        file is used again.  If the kernel fails to start the first time
-        though a new connection file will be used for the new kernel.
-        Parameters
-        ----------
-        now : bool, optional
-            If True, the kernel is forcefully restarted *immediately*, without
-            having a chance to do any cleanup action.  Otherwise the kernel is
-            given 1s to clean up before a forceful restart is issued.
-            In all cases the kernel is restarted, the only difference is whether
-            it is given a chance to perform a clean shutdown or not.
-        `**kw` : optional
-            Any options specified here will overwrite those used to launch the
-            kernel.
-        """
-        if self._launch_args is None:
-            raise RuntimeError("Cannot restart the kernel. "
-                               "No previous call to 'start_kernel'.")
-        else:
-            # Stop currently running kernel.
-            if self.has_kernel_started:
-                self.shutdown_kernel(now=now, restart=True)
-            else:
-                #wipe connection file as kernel has not started at all/for the first time
-                self.shutdown_kernel(now=now)
-
-            # Start new kernel.
-            self._launch_args.update(kw)
-            self.start_kernel(**self._launch_args)
