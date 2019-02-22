2431d38ab09ff83e3f7f205b472d12f8ba25d3bd
Kevin Bates <kbates4@gmail.com>
2019-02-22 13:50:21 -0800

    Support serving kernelspec resources
    A long-standing TODO item for NB2KG was to add support for kernelspec
    resources (kernel.js, kernel.css and logo-*).  This can't be accomplished
    until JKG provides the endpoint such that specific resources can be fetched.

--- kernel_gateway/services/kernelspecs/handlers.py
+++ kernel_gateway/services/kernelspecs/handlers.py
@@ -3,12 +3,19 @@
 """Tornado handlers for kernel specs."""
 
 import notebook.services.kernelspecs.handlers as notebook_handlers
+import notebook.kernelspecs.handlers as notebook_kernelspecs_resources_handlers
+
 from ...mixins import TokenAuthorizationMixin, CORSMixin, JSONErrorsMixin
 
 # Extends the default handlers from the notebook package with token auth, CORS
 # and JSON errors.
 default_handlers = []
 for path, cls in notebook_handlers.default_handlers:
+    # Everything should have CORS and token auth
+    bases = (TokenAuthorizationMixin, CORSMixin, JSONErrorsMixin, cls)
+    default_handlers.append((path, type(cls.__name__, bases, {})))
+
+for path, cls in notebook_kernelspecs_resources_handlers.default_handlers:
     # Everything should have CORS and token auth
     bases = (TokenAuthorizationMixin, CORSMixin, JSONErrorsMixin, cls)
     default_handlers.append((path, type(cls.__name__, bases, {})))
