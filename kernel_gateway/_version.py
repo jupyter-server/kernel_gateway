09a478c6256e8a1a373be29cdde79f47968b3e1e
Kevin Bates <kbates4@gmail.com>
2020-08-10 10:13:33 -0700

    Release 2.4.2

--- kernel_gateway/_version.py
+++ kernel_gateway/_version.py
@@ -3,11 +3,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (
-    2,
-    5,
-    0,
-    'dev'
-)
+version_info = (2, 4, 2)
 
 __version__ = '.'.join(map(str, version_info))
