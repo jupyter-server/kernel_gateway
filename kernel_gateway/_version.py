5269af0a2bf84b8a93b0c983d731b82843daec02
Kevin Bates <kbates4@gmail.com>
2023-02-06 11:35:09 -0800

    Set dev version

--- kernel_gateway/_version.py
+++ kernel_gateway/_version.py
@@ -3,6 +3,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (2, 5, 2)
+version_info = (2, 6, 0, 'dev0')
 
 __version__ = '.'.join(map(str, version_info))
