19be23564405e6e862b091b80ec73274c2588c82
Kevin Bates <kbates4@gmail.com>
2023-03-07 09:53:52 -0800

    Drop Python 3.7, add 3.10 & 3.11

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ containerized and scaled out using common technologies like
         'tornado>=6.2.0',
         'requests>=2.7,<3.0'
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
@@ -69,9 +69,10 @@ containerized and scaled out using common technologies like
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     include_package_data=True,
 )
