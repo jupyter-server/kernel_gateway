414cfe2e98da88d68358b50a618d580364100301
Zach Sailer <zsailer@apple.com>
2024-01-04 15:01:28 -0800

    root_dir defaults to current working directorY

--- kernel_gateway/services/kernels/manager.py
+++ kernel_gateway/services/kernels/manager.py
@@ -1,8 +1,9 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 """Kernel manager that optionally seeds kernel memory."""
-
+import os
 from typing import List, Optional
+from traitlets import default
 from jupyter_server.services.kernels.kernelmanager import AsyncMappingKernelManager
 from jupyter_client.ioloop import AsyncIOLoopKernelManager
 
@@ -15,8 +16,14 @@ class SeedingMappingKernelManager(AsyncMappingKernelManager):
     _seed_source: Optional[List]
     _seed_kernelspec: Optional[str]
 
+    @default("root_dir")
+    def _default_root_dir(self):
+        return os.getcwd()
+
     def _kernel_manager_class_default(self):
-        return 'kernel_gateway.services.kernels.manager.KernelGatewayIOLoopKernelManager'
+        return (
+            "kernel_gateway.services.kernels.manager.KernelGatewayIOLoopKernelManager"
+        )
 
     @property
     def seed_kernelspec(self) -> Optional[str]:
@@ -30,14 +37,16 @@ class SeedingMappingKernelManager(AsyncMappingKernelManager):
         str
             Name of the notebook kernelspec or None if no seed notebook exists
         """
-        if hasattr(self, '_seed_kernelspec'):
+        if hasattr(self, "_seed_kernelspec"):
             return self._seed_kernelspec
 
         if self.parent.seed_notebook:
             if self.parent.force_kernel_name:
                 self._seed_kernelspec = self.parent.force_kernel_name
             else:
-                self._seed_kernelspec = self.parent.seed_notebook['metadata']['kernelspec']['name']
+                self._seed_kernelspec = self.parent.seed_notebook["metadata"][
+                    "kernelspec"
+                ]["name"]
         else:
             self._seed_kernelspec = None
 
@@ -52,13 +61,14 @@ class SeedingMappingKernelManager(AsyncMappingKernelManager):
         list
             Notebook code cell contents or None if no seed notebook exists
         """
-        if hasattr(self, '_seed_source'):
+        if hasattr(self, "_seed_source"):
             return self._seed_source
 
         if self.parent.seed_notebook:
             self._seed_source = [
-                cell['source'] for cell in self.parent.seed_notebook.cells
-                if cell['cell_type'] == 'code'
+                cell["source"]
+                for cell in self.parent.seed_notebook.cells
+                if cell["cell_type"] == "code"
             ]
         else:
             self._seed_source = None
@@ -78,8 +88,10 @@ class SeedingMappingKernelManager(AsyncMappingKernelManager):
         seed notebook exists.
         """
         if self.parent.force_kernel_name:
-            kwargs['kernel_name'] = self.parent.force_kernel_name
-        kernel_id = await super(SeedingMappingKernelManager, self).start_kernel(*args, **kwargs)
+            kwargs["kernel_name"] = self.parent.force_kernel_name
+        kernel_id = await super(SeedingMappingKernelManager, self).start_kernel(
+            *args, **kwargs
+        )
 
         if kernel_id and self.seed_source is not None:
             # Only run source if the kernel spec matches the notebook kernel spec
@@ -104,13 +116,15 @@ class SeedingMappingKernelManager(AsyncMappingKernelManager):
                         msg_type = "kernel_info_reply"
                         while msg_type == "kernel_info_reply":
                             msg = await client.get_shell_msg()
-                            msg_type = msg['msg_type']
-                            if msg['content']['status'] != 'ok':
+                            msg_type = msg["msg_type"]
+                            if msg["content"]["status"] != "ok":
                                 # Shutdown the channels to remove any lingering ZMQ messages
                                 client.stop_channels()
                                 # Shutdown the kernel
                                 await self.shutdown_kernel(kernel_id)
-                                raise RuntimeError('Error seeding kernel memory', msg['content'])
+                                raise RuntimeError(
+                                    "Error seeding kernel memory", msg["content"]
+                                )
                 # Shutdown the channels to remove any lingering ZMQ messages
                 client.stop_channels()
         return kernel_id
@@ -121,14 +135,16 @@ class KernelGatewayIOLoopKernelManager(AsyncIOLoopKernelManager):
 
     Sets the environment variable 'KERNEL_GATEWAY' to '1' to indicate that the
     kernel is executing within a Jupyter Kernel Gateway instance. Removes the
-    KG_AUTH_TOKEN from the environment variables passed to the kernel when it 
+    KG_AUTH_TOKEN from the environment variables passed to the kernel when it
     starts.
     """
 
     async def _async_launch_kernel(self, kernel_cmd, **kw):
         # TODO - should probably figure out a better place to deal with this
-        env = kw['env']
-        env['KERNEL_GATEWAY'] = '1'
-        if 'KG_AUTH_TOKEN' in env:
-            del env['KG_AUTH_TOKEN']
-        return await super(KernelGatewayIOLoopKernelManager, self)._async_launch_kernel(kernel_cmd, **kw)
+        env = kw["env"]
+        env["KERNEL_GATEWAY"] = "1"
+        if "KG_AUTH_TOKEN" in env:
+            del env["KG_AUTH_TOKEN"]
+        return await super(KernelGatewayIOLoopKernelManager, self)._async_launch_kernel(
+            kernel_cmd, **kw
+        )
